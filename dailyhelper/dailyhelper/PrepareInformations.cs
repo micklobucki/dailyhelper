1b8a7256225f2424c1b327fc3e3c8ef794ccfb9d
mk <mefisto506@gmail.com>
2017-08-06 22:48:55 +0200

    get and show weather img

---
+++ dailyhelper/dailyhelper/PrepareInformations.cs
@@ -0,0 +1,31 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace dailyhelper
+{
+    static class PrepareInformations
+    {
+        public static BitmapImage getWeather()
+        {
+            var html = @"http://lodz.infometeo.pl/";
+
+            HtmlWeb web = new HtmlWeb();
+
+            var htmlDoc = web.Load(html);
+
+            string hrefWeather = htmlDoc.GetElementbyId("icm60h-meteogram").GetAttributeValue("src", "");
+            hrefWeather = hrefWeather.Replace("&amp;", "&");
+
+            BitmapImage weatherValue = new BitmapImage();
+            weatherValue.BeginInit();
+            weatherValue.UriSource = new Uri(hrefWeather);
+            weatherValue.EndInit();
+            return weatherValue;
+        }
+    }
+}
